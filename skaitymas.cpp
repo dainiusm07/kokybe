bc6e8fe3aab774bff23981477681bfe58a5bf24a
dainiusm07 <dainius.masevicius@gmail.com>
2021-11-04 20:05:44 +0200

    Code review fix

--- skaitymas.cpp
+++ skaitymas.cpp
@@ -1,46 +1,44 @@
-void Skaitymas (int t, int & m, std::vector<duom>& Duomenys){
-    std::string s = std::to_string(t);
-    std::ifstream fd("test" + s + ".md");
+void skaitymas (int file_nr, int & m, std::vector<duom>& Duomenys){
+    std::string stud_nr = std::to_string(t);
+    std::ifstream fd("test" + stud_nr + ".md");
     if (!fd.good()){
         throw "Failai neegzistuoja";
     }
     int laik;
-    std::string pvz;
-    std::vector<int> Medv;
+    std::string first_line;
+    std::vector<int> median_vector;
     while (!fd.eof()){
         
         int sum = 0, kas = 0;
         Duomenys.push_back(duom());
-        getline(fd, pvz);
-        std::istringstream in_line(pvz);
+        getline(fd, first_line);
+        std::istringstream in_line(first_line);
         try {
             in_line >> Duomenys[m].var;
-            //Patikra (Duomenys[m].var);
             in_line >> Duomenys[m].pav;
-            //Patikra (Duomenys[m].pav);
         } catch (const char* msg){
             cout << msg << endl;
         }
         while (in_line >> laik){
             if (laik == 0){
-                cout << "Blogai ivesti duomenys pas studenta: " << Duomenys[m].var << ", " << t << "-ame faile" << endl;
+                cout << "Blogai ivesti duomenys pas studenta: " << Duomenys[m].var << ", " << file_nr << "-ame faile" << endl;
                 sum = 0;
                 break;
             }
             sum = sum + laik;
-            Medv.push_back(int());
-            Medv[kas] = laik;
+            median_vector.push_back(int());
+            median_vector[kas] = laik;
             kas++;
         }
 
-        if (Medv.empty()){
-            cout << "Blogai ivesti duomenys " << t << "-ame faile" << endl;
+        if (median_vector.empty()){
+            cout << "Blogai ivesti duomenys " << file_nr << "-ame faile" << endl;
             break;
         }
-        int egz = Medv[kas-1];
+        int egz = median_vector[kas-1];
         kas--;
         sum = sum - egz;
-        Skaiciavimai (Duomenys, m, kas, Medv, sum, egz);
+        Skaiciavimai (Duomenys, m, kas, median_vector, sum, egz);
         m++;
     } 
     fd.close();
